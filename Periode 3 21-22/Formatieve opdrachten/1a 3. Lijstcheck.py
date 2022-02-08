f5a4fbf520ca55d39a9f1750e82b05b170167f64
SelfmadeRES <rickemir@gmail.com>
2022-02-08 15:15:10 +0100

    Create 1a 3. Lijstcheck.py
    1a 3. Lijstcheck gemaakt

---
+++ Periode 3 21-22/Formatieve opdrachten/1a 3. Lijstcheck.py
@@ -0,0 +1,40 @@
+#Opdracht 3 - Lijstcheck
+#a. Schrijf een functie count() die berekent hoe vaak een geheel getal x in een lijst voorkomt.
+
+def count(x, list):
+    xCount = 0
+    for char in list:
+        if char == x:       #Is x gelijk aan het geïtereerde character
+            xCount += 1
+    return xCount
+#b. Schrijf een functie die in een gegeven lijst het grootste verschil tussen twee op een volgende getallen bepaalt.
+
+def biggest_difference(list):
+    min = 99999999999999999999999999999
+    max = -99999999999999999999999999999
+    for char in list:
+        if char < min:
+            min = char
+        if char > max:
+            max = char
+    return abs(min - max)
+
+#c. Schrijf een functie, die bepaalt of een gegeven lijst met alleen 1’en en 0’en aan de volgende eisen voldoet:
+  #- Het aantal enen is groter dan aan het aantal nullen
+  #- Er mogen niet meer dan 12 nullen zijn.
+#Bedenk zelf wat het return type van deze functie moet zijn. Gebruik in je programma de functie count() die je hebt geschreven bij de vorige opgave.
+
+def binary_check(list):
+    zeroCount = 0
+    oneCount = 0
+    for number in list:
+        if number == 0:         #Als het getal een 0 is, voeg toe aan zeroCount, anders is het een 1 en dus voeg je dan toe aan oneCount
+            zeroCount += 1
+        else:
+            oneCount += 1
+    return oneCount > zeroCount and zeroCount <= 12     #Condities worden gecheckt en de returnwaarde is een boolean
+
+
+print(count(3, [1, 12, 3, 5, 45, 8, 3, 56, 3, 3, 1, 5, 3]))
+print(biggest_difference([1, 12, 3, 5, 45, 8, 3, 56, 3, 3, 1, 5, 3]))
+print(binary_check([0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1]))
