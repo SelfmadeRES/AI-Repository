d1162c27d6d9fd1ed4a3b3b61f49dcc962e68fdc
SelfmadeRES <rickemir@gmail.com>
2022-02-10 21:14:05 +0100

    Begin gemaakt aan 8 en 9
    Begin gemaakt voor opdrachten 8 en 9, maar beide functie zijn nog niet werkend

--- Periode 3 21-22/Formatieve opdrachten/1a 8. Compressie.py
+++ Periode 3 21-22/Formatieve opdrachten/1a 8. Compressie.py
@@ -4,3 +4,21 @@
 #Verder zijn alle lege regels verwijderd (een lege regel bevat ’\n’ , eventueel voorafgegaan door spaties en tabs(‘\t’)).
 
 def compress():
+    f = open("ToCompress.txt")
+    f.readlines()
+    f.close()
+    done = False
+    charIndex = 0
+    lineIndex = 0
+    while done == False:
+        if f[lineIndex][charIndex] == "" or f[lineIndex][charIndex] == "\n" or f[lineIndex][charIndex] == "\t":
+            #remove char from line
+            charIndex += 1
+        else:
+            lineIndex += 1
+            charIndex = 0
+        if lineIndex == len(f):
+            done = True
+    return
+
+#Functie werkt nog niet
