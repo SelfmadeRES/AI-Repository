92b510d063b8e936231731c1ddb2211ce8c6143a
SelfmadeRES <rickemir@gmail.com>
2022-02-11 15:44:23 +0100

    Create 1b Opstart Mastermind.py
    opzetten opstart

---
+++ Periode 3 21-22/Formatieve opdrachten/1b Opstart Mastermind.py
@@ -0,0 +1,8 @@
+#opstart mastermind
+# Het spel heeft 6 kleuren, en je hebt 10 zetten
+# Kleuren kunnen 2 om meer keer voorkomen in de code
+# Er zijn dus 6**4 mogelijke antwoorden = 1296
+
+#Gebruikte libraries
+import random
+import itertools
