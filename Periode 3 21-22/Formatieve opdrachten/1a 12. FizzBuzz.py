70efc73a3516a8a9b55a25025228ca7b0f075d17
SelfmadeRES <rickemir@gmail.com>
2022-02-11 14:11:13 +0100

    Create 1a 12. FizzBuzz.py
    Opdracht 12. FizzBuzz gemaakt

---
+++ Periode 3 21-22/Formatieve opdrachten/1a 12. FizzBuzz.py
@@ -0,0 +1,14 @@
+#Opdracht 12 - FizzBuzz
+#Schrijf een programma dat de getallen 1 tot 100 print,
+#maar print voor veelvouden van drie “fizz” in plaats van het getal en voor veelvouden van vijf print “buzz” in plaats van
+#het getal. Getallen die zowel veelvoud zijn van drie als van vijf worden afgedrukt als “fizzbuzz”
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
