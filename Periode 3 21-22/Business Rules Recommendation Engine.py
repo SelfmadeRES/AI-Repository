086968fc43c3b4fd46c9fb12ccdc7176fcce1109
SelfmadeRES <rickemir@gmail.com>
2022-04-15 13:58:58 +0200

    Create Business Rules Recommendation Engine.py
    Create Business Rules

---
+++ Periode 3 21-22/Business Rules Recommendation Engine.py
@@ -0,0 +1,40 @@
+"""Opdracht Business Rules Recommendation Engine
+Rick Schellevis V1B
+1790130"""
+
+"""In dit bestand beschrijf ik de business rules die we als projectgroepje gaan gebruiken bij het schrijven van de algoritmes voor de recommendation engine"""
+
+#Stap 1: Het idee
+"""Allereerst begint het algoritme met een idee. Dit idee kan gekomen zijn omdat we het algoritme hebben gezien op een andere website, maar het kan ook
+compleet zelf bedacht zijn."""
+
+#Stap 2: Het idee uitwerken
+"""Daarna wordt het idee verder uitgewerkt. Er moet worden bedacht waar op de website het algoritme moet werken, welke dat we gaan gebruiken, en wat
+precies het doel moet zijn van het algoritme."""
+
+#Tussenstap PO
+"""In deze tussenstap moet worden overlegd met de PO of hij het algoritme ook een goed idee vindt en of hij het eens is met onze uitwerking."""
+
+#Stap 3: Pseudocode
+"""Nu het idee is goedgekeurd is het tijd om pseudocode te schrijven voor het algoritme. Dit is een stapgewijze beschrijving van hoe de code in elkaar moet 
+zitten. Wel zou de pseudocode in principe gelezen moeten kunnen worden zonder technische achtergrond."""
+
+#Stap 4: Predikaatlogica
+"""Naast de Pseudocode wordt ook predikaatlogica geschreven voor het algoritme. Zo krijgen we als groep een beter inzicht op welke data we willen gebruiken,
+en wat voor soort functie we willen los laten om zo onze gewenste recommendations te krijgen. Predikaatlogica hoeft niet leesbaar te zijn voor iemand
+zonder technische achtergrond. Ook hoeven de functies nog niet in 1x goed te zijn, later wordt dit nog getest."""
+
+#Stap 5: Schrijven code
+"""Nu wordt aan de hand van de geschreven pseudocode en predikaatlogica de code daadwerkelijk geschreven. Eventuele oversights worden weggewerkt en na deze 
+stap zou er een werkend algoritme moeten ontstaan"""
+
+#Stap 6: Testen code
+"""Nu is het tijd om te code te testen. Het kan natuurlijk best zijn dat met de functies van de eerder geschreven predikaatlogica de recommendations nog niet naar
+wens zijn. Dan worden de functies aangepast en opnieuw getest, totdat het groepje tevreden is."""
+
+#Tussenstap PO
+"""Als het groepje tevreden is, dan kunnen zij de code voorleggen aan de PO. Het kan namelijk nog steeds best zijn dat het toch nog niet naar wens is voor de PO.
+Mocht dit het geval zijn, dan keert het groepje terug naar Stap 6 met nieuw verkregen feedback. Hierna zal het opnieuw gekeurd worden door de PO"""
+
+#Stap 7: Advies schrijven
+"""Als de recommendations zijn goedgekeurd hoeft het groepje alleen nog een advies op te stellen aan de hand van de code. Zo is alles duidelijk voor de opdrachtgever"""
